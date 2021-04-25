de7e55844665fa51c744aaaddc39f2038313d2f6
Pragdeesh-stack <81550397+Pragdeesh-stack@users.noreply.github.com>
2021-04-25 10:58:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/c-editor')
+def editor():
+    return render_template('codeeditor.html')
+if __name__ == "__main__":
+    app.run()
